448b46c0ff9f67556c5987ae79a74044e3c686f3
David Viuche <jviucheroja@uniminuto.edu.co>
2020-05-18 12:32:54 -0500

    iniciar sesion terminado

--- back/src/controllers/sesionController.js
+++ back/src/controllers/sesionController.js
@@ -4,25 +4,56 @@ const controller = {}
 
 controller.iniciarSesion = (req, res) => {
     const { correo, contraseña, tipo } = req.body;
-    if(tipo == 'empleado'){
-        res.send('empleado');
-    }else if(tipo == 'usuario'){
-        
+    if (tipo == 'empleado') {
+        Empleado.findOne({ where: { correo } })
+            .then(result => {
+                if (!result) {
+                    res.statusCode = 404;
+                    res.json({ error: 'correo incorrecto' });
+                } else {
+                    if (result.contraseña == contraseña) {
+                        res.json(result);
+                    } else {
+                        res.statusCode = 404;
+                        res.json({ error: 'contraseña incorrecta' });
+                    }
+                }
+            })
+    } else if (tipo == 'usuario') {
+        Usuario.findOne({ where: { correo } })
+            .then(result => {
+                if (!result) {
+                    res.statusCode = 404;
+                    res.json({ error: 'correo incorrecto' });
+                } else {
+                    if (result.contraseña == contraseña) {
+                        res.json(result);
+                    } else {
+                        res.statusCode = 404;
+                        res.json({ error: 'contraseña incorrecta' });
+                    }
+                }
+            })
     }
 }
 
-controller.registrarse = (req, res)=>{
-    const { correo, contraseña, tipo } = req.body;
-    if(tipo == 'empleado'){
-        Empleado.create({correo,contraseña})
-        .then(result=>{
-            res.json(result);
-        })
-    }else if(tipo == 'usuario'){
-        Usuario.create({correo,contraseña})
-        .then(result=>{
-            res.json(result);
-        })
+controller.registrarse = (req, res) => {
+    const { correo, contraseña, tipo, nivelAcceso } = req.body;
+    if (tipo == 'empleado') {
+        if (correo && contraseña && nivelAcceso) {
+            Empleado.create({ correo, contraseña, nivelAcceso })
+                .then(result => {
+                    res.json(result);
+                })
+        } else {
+            res.statusCode = 400;
+            res.json({ error: 'bad request' });
+        }
+    } else if (tipo == 'usuario') {
+        Usuario.create({ correo, contraseña })
+            .then(result => {
+                res.json(result);
+            })
     }
 }
 
