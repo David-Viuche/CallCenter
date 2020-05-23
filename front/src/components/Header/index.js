e1a258292ea0ca6c7cf9517d9b4477ba26f689e0
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-05-23 17:26:18 -0500

    Estilar botón de cerrar sesión

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Logo } from '../Logo';
 import { Nav, Line } from './styles';
+import { Context } from '../../Context';
 
 export const Header = () => {
+
+    const { removeAuth, isAuth } = useContext(Context);
+
+    const handleClick = (e) => {
+        const id = window.sessionStorage.getItem('id');
+        removeAuth(id);
+    }
+
     return (
         <React.Fragment>
             <Nav>
                 <Logo />
+                {isAuth && <button onClick={handleClick}>Cerrar sesion</button>}
             </Nav>
             <Line />
         </React.Fragment>
